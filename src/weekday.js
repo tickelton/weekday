40c82df16a22dd543ae043f62efe6662844a4bf3
tickelton <tickelton@gmail.com>
2015-04-27 08:45:10 +0200

    initial pages commit

---
+++ src/weekday.js
@@ -0,0 +1,16 @@
+function updateClock() {
+	var hand = document.getElementById("src_img").contentDocument.getElementById("g3007");
+	var d = new Date();
+	var hours = d.getHours();
+	var minutes = d.getMinutes();
+	var day = d.getDay();
+	
+	hand.setAttribute('transform', 'rotate('+
+				(((day*24*60)+(hours*60)+minutes)/(10080/360)-25.7) +
+				' 317 309)');
+}
+
+function setClockTimer() {
+	updateClock()
+	setInterval(updateClock, 60000) 
+}
