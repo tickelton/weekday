3f39770cef99d96559ddbb25133c38c7b9e559f1
tickelton <tickelton@gmail.com>
2015-05-07 12:05:53 +0200

    fixed fonts
     * converted fonts in SVG to path
     * fixed id of hand object

--- src/weekday.js
+++ src/weekday.js
@@ -1,13 +1,13 @@
 function updateClock() {
-	var hand = document.getElementById("src_img").contentDocument.getElementById("g3007");
+	var hand = document.getElementById("src_img").contentDocument.getElementById("g4261");
 	var d = new Date();
 	var hours = d.getHours();
 	var minutes = d.getMinutes();
 	var day = d.getDay();
 	
-	hand.setAttribute('transform', 'rotate('+
-				(((day*24*60)+(hours*60)+minutes)/(10080/360)-25.7) +
-				' 300 300)');
+	hand.setAttribute('transform', 'translate(-500,0) rotate('+
+				(((day*24*60)+(hours*60)+minutes)/(10080/360)-25.7)+
+				' 800 300)');
 }
 
 function setClockTimer() {
